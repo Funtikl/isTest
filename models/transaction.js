0b22e3ffd94a62fff13b8abe8da42988d900238a
Fuad Rustamzadeh <rustam.fuad@gmail.com>
2019-02-27 12:41:27 +0400

    almost done

--- models/transaction.js
+++ models/transaction.js
@@ -2,8 +2,6 @@
 const User = require('/Users/fuad/Code/isTest/isTest/models/user.js');
 module.exports = (sequelize, DataTypes) => {
   const Transaction = sequelize.define('Transaction', {
-    
-    
     amount: DataTypes.INTEGER
   }, {});
   Transaction.associate = function(models) {
